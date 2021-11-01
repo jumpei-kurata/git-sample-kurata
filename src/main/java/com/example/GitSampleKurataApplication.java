dbd6d505a8e6a1f7a0c70a2b32029696c26dff87
jumpei-kurata <jumpei.kurata@rakus-partners.co.jp>
2021-11-01 14:34:28 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleKurataApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKurataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKurataApplication.class, args);
+	}
+
+}
